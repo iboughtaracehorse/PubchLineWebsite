23b6995b11bd586ad3083fc550bb75ee4dac016c
Dariia Dushynska <ddushynska@kse.org.ua>
2024-10-18 19:35:11 +0300

    api url added. small updated in code

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-let elt = document.querySelectorAll('.sliding-title > *')
+let element = document.querySelectorAll('.sliding-title > *')
 
 anime({
-  targets: elt,
+  targets: element,
   translateX: '-100%',
-  duration: 1200,
+  duration: 10000,
   easing: 'linear',
   loop: true
 });
+
+
+
+const URL = "https://official-joke-api.appspot.com/jokes/random/27"
+
+
+
+
+
+
+
+
+
